551944445e02184f65101aea1a97fb1b50cde819
MDNich <71262643+MDNich@users.noreply.github.com>
2025-09-01 13:15:31 -0400

    Create preLaunch_windows.sh

---
+++ sim/test/preLaunch_windows.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+export PYTHONPATH="./" && /c/Users/cster/AppData/Local/Programs/Python/Python312/python.exe /c/Users/cster/Documents/Github/ActiveControl_MIT_RktTeam/sim/src/py/test/openRocketInterface.py
