5575dca24a01fdb8f3a02d4f975e465fad8865a1
MDNich <71262643+MDNich@users.noreply.github.com>
2025-09-07 15:06:45 -0400

    Update Splash.java

--- clone/openrocket-release-24.12.RC.01/swing/src/main/java/info/openrocket/swing/gui/main/Splash.java
+++ clone/openrocket-release-24.12.RC.01/swing/src/main/java/info/openrocket/swing/gui/main/Splash.java
@@ -62,7 +62,7 @@ public class Splash {
 	
 	
 	private static void drawVersionNumber(Graphics2D g2) {
-		String text = "Version " + BuildProperties.getVersion();
+		String text = "MIT Edition (v2)";
 		GlyphVector gv = VERSION_FONT.createGlyphVector(g2.getFontRenderContext(), text);
 		
 		Rectangle2D rect = gv.getVisualBounds();
