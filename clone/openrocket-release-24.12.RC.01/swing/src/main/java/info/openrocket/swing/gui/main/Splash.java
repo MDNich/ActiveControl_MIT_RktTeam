240e444639deafd5fb3f087a134e3c89cbe3c6a3
MDNich <71262643+MDNich@users.noreply.github.com>
2025-11-18 11:43:33 -0500

    Update version text in Splash screen to v5
    Changed the displayed version in Splash.java from 'MIT Edition (v4)' to 'MIT Edition (v5)'.

--- clone/openrocket-release-24.12.RC.01/swing/src/main/java/info/openrocket/swing/gui/main/Splash.java
+++ clone/openrocket-release-24.12.RC.01/swing/src/main/java/info/openrocket/swing/gui/main/Splash.java
@@ -62,7 +62,7 @@ public class Splash {
 	
 	
 	private static void drawVersionNumber(Graphics2D g2) {
-		String text = "MIT Edition (v4)";
+		String text = "MIT Edition (v5)";
 		GlyphVector gv = VERSION_FONT.createGlyphVector(g2.getFontRenderContext(), text);
 		
 		Rectangle2D rect = gv.getVisualBounds();
