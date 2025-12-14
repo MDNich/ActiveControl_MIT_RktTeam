37241aa9304f34e1f08c5fbabdb8fdb1d102eeff
MDNich <71262643+MDNich@users.noreply.github.com>
2025-12-14 13:02:45 -0500

    Refactor sensor data fudging in flight controller sim
    Centralized and modularized the logic for generating 'fudged' sensor data in FlightControllerSimulatorListener by introducing a master fudgeSimulationStatus method and dedicated per-sensor fudging functions. Updated RTFC to consume the new extraData keys for simulated sensor values, improving maintainability and extensibility of the sensor simulation pipeline.

--- clone/openrocket-release-24.12.RC.01/core/src/main/java/edu/mit/rocket_team/zephyrus/FC/RTFC.java
+++ clone/openrocket-release-24.12.RC.01/core/src/main/java/edu/mit/rocket_team/zephyrus/FC/RTFC.java
@@ -7,6 +7,7 @@ import info.openrocket.core.models.atmosphere.AtmosphericConditions;
 import info.openrocket.core.simulation.FlightDataType;
 import info.openrocket.core.simulation.SimulationStatus;
 import info.openrocket.core.util.Coordinate;
+import info.openrocket.core.util.WorldCoordinate;
 
 import java.util.List;
 
@@ -41,35 +42,42 @@ public class RTFC {
     public static void pre_loop(SimulationStatus fudgedStat) {
         currentFCsimStat = fudgedStat.clone();
 
-        List<Double> accelZ = currentFCsimStat.getFlightDataBranch().get(FlightDataType.TYPE_ACCELERATION_Z);
-        List<Double> accelXY = currentFCsimStat.getFlightDataBranch().get(FlightDataType.TYPE_ACCELERATION_XY);
-
+        // accel
+        Coordinate measuredAccel = (Coordinate) currentFCsimStat.getExtraData("fudged_accel");
         // mag
         Coordinate worldAngle = (Coordinate) currentFCsimStat.getExtraData("fudged_world_angle");
         // gyro
         Coordinate worldAngleRate = (Coordinate) currentFCsimStat.getExtraData("fudged_world_angle_rate");
 
-        // pressure
+        // baro
         double alt = currentFCsimStat.getRocketWorldPosition().getAltitude();
         AtmosphericConditions atmos = currentFCsimStat.getSimulationConditions().
                 getAtmosphericModel().
                 getConditions(alt);
 
+        // GPS
+        WorldCoordinate pos = (WorldCoordinate) currentFCsimStat.getExtraData("fudged_gps_position");
+        boolean hasFix = (Boolean) currentFCsimStat.getExtraData("fudged_gps_has_fix");
+
         RTAccelData accelDat = new RTAccelData(
-                accelXY.get(accelXY.size() - 1),
-                accelXY.get(accelXY.size() - 1),
-                accelZ.get(accelZ.size() - 1));
+                measuredAccel.x,
+                measuredAccel.y,
+                measuredAccel.z
+        );
         RTBaroData baroDat = new RTBaroData(
-                atmos.getPressure(),
-                atmos.getTemperature(),
-                atmos.getTemperature(),
-                alt, // fudged already, intended for barometer
-                atmos.getPressure());
+                (Double) currentFCsimStat.getExtraData("fudged_rawPressure"),
+                (Double) currentFCsimStat.getExtraData("fudged_rawTemperature"),
+                (Double) currentFCsimStat.getExtraData("fudged_temperature"),
+                (Double) currentFCsimStat.getExtraData("fudged_altitude"),
+                (Double) currentFCsimStat.getExtraData("fudged_pressure")
+        );
         RTGPSData gpsDat = new RTGPSData(
-                currentFCsimStat.getRocketWorldPosition().getLatitudeDeg(),
-                currentFCsimStat.getRocketWorldPosition().getLongitudeDeg(),
-                (Double) currentFCsimStat.getExtraData("fudged_gps_altitude"),
-                1.0,1.0,1.0,(Boolean) currentFCsimStat.getExtraData("fudged_gps_has_fix"));
+                pos.getLatitudeDeg(),
+                pos.getLongitudeDeg(),
+                pos.getAltitude(),
+                1.0,1.0,1.0,
+                hasFix
+        );
         RTGyroData gyroDat = new RTGyroData(
                 worldAngleRate.x,
                 worldAngleRate.y,
--- clone/openrocket-release-24.12.RC.01/core/src/main/java/info/openrocket/core/simulation/listeners/FlightControllerSimulatorListener.java
+++ clone/openrocket-release-24.12.RC.01/core/src/main/java/info/openrocket/core/simulation/listeners/FlightControllerSimulatorListener.java
@@ -1,6 +1,7 @@
 package info.openrocket.core.simulation.listeners;
 
 import edu.mit.rocket_team.zephyrus.FC.RTFC;
+import info.openrocket.core.models.atmosphere.AtmosphericConditions;
 import info.openrocket.core.rocketcomponent.Rocket;
 import info.openrocket.core.rocketcomponent.RocketComponent;
 import info.openrocket.core.rocketcomponent.TabControlledTrapezoidFinSet;
@@ -112,17 +113,12 @@ public class FlightControllerSimulatorListener extends AbstractSimulationListene
         double finTimeStep = status.getSimulationTime();
         latestTimeStep = finTimeStep - loopStart;
 
-        double current_fudged_altitude = status.getRocketWorldPosition().getAltitude() + (0.5-random())*2*amplitude_randomness_size;
-        altitudeMeasuredList.add(current_fudged_altitude);
         double realVelocity = status.getRocketVelocity().length();
-        double gps_fudged_altitude = Math.round(current_fudged_altitude*100.0)/100.0;
-        boolean gps_has_fix = true;
 
         pastOmegaZ.add(status.getRocketRotationVelocity().z);
         pastThetaZ.add(toDegrees(toEulerAngles_rocketCoord(status.getRocketOrientationQuaternion()).z));
         finTabAngleLog.add(getFinTabAngleDeg());
         rktVelMagLog.add(realVelocity);
-        rktAltLog.add(current_fudged_altitude);
 
         List<Double> CldFlightBranch = status.getFlightDataBranch().get(FlightDataType.TYPE_ROLL_DAMPING_COEFF);
         CldLog.add(CldFlightBranch.get(CldFlightBranch.toArray().length-1));
@@ -133,40 +129,10 @@ public class FlightControllerSimulatorListener extends AbstractSimulationListene
         double currentTime = status.getSimulationTime();
         System.out.println("[JAVA] current time " + currentTime + "                     \r");
 
+
+        // fudging
         SimulationStatus fudgedStatus = status.clone();
-
-        WorldCoordinate realLocation = status.getRocketWorldPosition();
-        fudgedStatus.setRocketWorldPosition(new WorldCoordinate(
-                realLocation.getLatitudeRad(),
-                realLocation.getLongitudeRad(),
-                current_fudged_altitude));
-        fudgedStatus.putExtraData("fudged_gps_altitude", gps_fudged_altitude);
-        fudgedStatus.putExtraData("fudged_gps_has_fix", gps_has_fix);
-
-        // Fudge the orientation
-
-        List<Double> rollAngles = fudgedStatus.getFlightDataBranch().get(FlightDataType.TYPE_ORIENTATION_PHI);
-        List<Double> pitchAngles = fudgedStatus.getFlightDataBranch().get(FlightDataType.TYPE_ORIENTATION_THETA);
-
-        double rollAngle = rollAngles.get(rollAngles.size() - 1);
-        double pitchAngle = pitchAngles.get(pitchAngles.size() - 1);
-        Coordinate worldAngle = convertToImuAngles(rollAngle, pitchAngle);
-        // TODO Fudge the world angles as needed !
-
-        fudgedStatus.putExtraData("fudged_world_angle", worldAngle);
-
-        List<Double> rollRates = fudgedStatus.getFlightDataBranch().get(FlightDataType.TYPE_ROLL_RATE);
-        List<Double> pitchRates = fudgedStatus.getFlightDataBranch().get(FlightDataType.TYPE_PITCH_RATE);
-
-        double rollRate = rollRates.get(rollRates.size() - 1);
-        double pitchRate = pitchRates.get(pitchRates.size() - 1);
-        Coordinate worldAngRate = convertToImuAngles(rollRate, pitchRate);
-        // TODO Fudge the world angular vels as needed !
-
-        fudgedStatus.putExtraData("fudged_world_angle_rate", worldAngRate);
-
-
-
+        fudgeSimulationStatus(fudgedStatus);
 
         RTFC.pre_loop(fudgedStatus.clone());
         RTFC.loop();
@@ -223,6 +189,131 @@ public class FlightControllerSimulatorListener extends AbstractSimulationListene
     }
 
 
+    // Master Fudger
+
+    public static void fudgeSimulationStatus(SimulationStatus fudgedStatus) {
+        // accel
+        List<Double> accelZ = fudgedStatus.getFlightDataBranch().get(FlightDataType.TYPE_ACCELERATION_Z);
+        List<Double> accelXY = fudgedStatus.getFlightDataBranch().get(FlightDataType.TYPE_ACCELERATION_XY);
+
+        Coordinate realAccel = new Coordinate(
+                accelXY.get(accelXY.size() - 1),
+                accelXY.get(accelXY.size() - 1),
+                accelZ.get(accelZ.size() - 1));
+        Coordinate fudgedAccel = fudgeAccel(realAccel);
+        fudgedStatus.putExtraData("fudged_accel", fudgedAccel);
+
+        // baro
+
+        double alt = fudgedStatus.getRocketWorldPosition().getAltitude();
+        AtmosphericConditions atmos = fudgedStatus.getSimulationConditions().
+                getAtmosphericModel().
+                getConditions(alt);
+
+        double rP0 = atmos.getPressure();
+        double rT0 = atmos.getTemperature();
+        double t0 = atmos.getTemperature();
+        double a0 = alt;
+        double p0 = atmos.getPressure();
+
+        double rP1 = fudgeRawPressure(rP0);
+        double rT1 = fudgeRawTemperature(rT0);
+        double t1 = fudgeTemperature(t0);
+        double a1 = fudgeAltitude(alt);
+        double p1 = fudgePressure(p0);
+
+        fudgedStatus.putExtraData("fudged_rawPressure", rP1);
+        fudgedStatus.putExtraData("fudged_rawTemperature", rT1);
+        fudgedStatus.putExtraData("fudged_temperature", t1);
+        fudgedStatus.putExtraData("fudged_altitude", a1);
+        fudgedStatus.putExtraData("fudged_pressure", p1);
+
+        // GPS
+        WorldCoordinate realLocation = fudgedStatus.getRocketWorldPosition();
+        WorldCoordinate fudgedLocation = fudgeGPS(realLocation);
+        boolean gps_has_fix = true; // possibly fudge this too.
+        fudgedStatus.putExtraData("fudged_gps_has_fix", gps_has_fix);
+        fudgedStatus.putExtraData("fudged_gps_position", fudgedLocation);
+
+        // Fudge the orientation
+
+        List<Double> rollAngles = fudgedStatus.getFlightDataBranch().get(FlightDataType.TYPE_ORIENTATION_PHI);
+        List<Double> pitchAngles = fudgedStatus.getFlightDataBranch().get(FlightDataType.TYPE_ORIENTATION_THETA);
+
+        double rollAngle = rollAngles.get(rollAngles.size() - 1);
+        double pitchAngle = pitchAngles.get(pitchAngles.size() - 1);
+        Coordinate worldAngle = convertToImuAngles(rollAngle, pitchAngle);
+        Coordinate fudgedWorldAngle = fudgeWorldAngle(worldAngle);
+        fudgedStatus.putExtraData("fudged_world_angle", fudgedWorldAngle);
+
+        List<Double> rollRates = fudgedStatus.getFlightDataBranch().get(FlightDataType.TYPE_ROLL_RATE);
+        List<Double> pitchRates = fudgedStatus.getFlightDataBranch().get(FlightDataType.TYPE_PITCH_RATE);
+
+        double rollRate = rollRates.get(rollRates.size() - 1);
+        double pitchRate = pitchRates.get(pitchRates.size() - 1);
+        Coordinate worldAngRate = convertToImuAngles(rollRate, pitchRate);
+        Coordinate fudgedWorldAngleRate = fudgeWorldAngleRate(worldAngRate);
+        fudgedStatus.putExtraData("fudged_world_angle_rate", fudgedWorldAngleRate);
+
+        // no return, its a shared reference
+    }
+
+
+
+
+
+    // Fudgers per sensor
+    // Accel
+    public static Coordinate fudgeAccel(Coordinate accel) {
+        // for the moment no-op.
+        return accel;
+    }
+
+    // Baro
+    public static double fudgeAltitude(double altitude) {
+        // fudge with ± amplitude_randomness_size meters
+        return altitude + (0.5-random())*2*amplitude_randomness_size;
+    }
+    public static double fudgeRawPressure(double rP0) {
+        // for the moment no-op.
+        return rP0;
+    }
+    public static double fudgeRawTemperature(double rT0) {
+        // for the moment no-op.
+        return rT0;
+    }
+    public static double fudgeTemperature(double t0) {
+        // for the moment no-op.
+        return t0;
+    }
+    public static double fudgePressure(double p0) {
+        // for the moment no-op.
+        return p0;
+    }
+
+    // GPS
+    public static WorldCoordinate fudgeGPS(WorldCoordinate location) {
+        // for the moment no-op.
+        return new WorldCoordinate(
+                location.getLatitudeRad(),
+                location.getLongitudeRad(),
+                location.getAltitude() // GPS-specific altitude fudging, not shared with baro.
+        );
+    }
+
+    // Mag / IMU
+    public static Coordinate fudgeWorldAngle(Coordinate worldAngle) {
+        // for the moment no-op.
+        return worldAngle;
+    }
+
+    // Gyro
+    public static Coordinate fudgeWorldAngleRate(Coordinate worldAngleRate) {
+        // for the moment no-op.
+        return worldAngleRate;
+    }
+
+
 
 
 
@@ -240,31 +331,6 @@ public class FlightControllerSimulatorListener extends AbstractSimulationListene
         double angleY = 2 * atan2(sinp, cosp) - PI / 2;
 
         // yaw (z-axis rotation)
-        double siny_cosp = 2 * (q.getW() * q.getZ() + q.getX() * q.getY());
-        double cosy_cosp = 1 - 2 * (q.getY() * q.getY() + q.getZ() * q.getZ());
-        double angleZ = atan2(siny_cosp, cosy_cosp);
-
-        if (roundToNearest5) {
-            angleX = round(angleX / (Math.PI / 180 * 1));// * (Math.PI / 180 * 5);
-            angleY = round(angleY / (Math.PI / 180 * 1));// * (Math.PI / 180 * 5);
-            angleZ = round(angleZ / (Math.PI / 180 * 1));// * (Math.PI / 180 * 5);
-        }
-
-        return new Coordinate(angleX, angleY, angleZ);
-    }
-    public static Coordinate toEulerAngles_worldCoord(Quaternion q) {
-
-        // roll (x-axis rotation) -> should be assigned to z
-        double sinr_cosp = 2 * (q.getW() * q.getX() + q.getY() * q.getZ());
-        double cosr_cosp = 1 - 2 * (q.getX() * q.getX() + q.getY() * q.getY());
-        double angleX = atan2(sinr_cosp, cosr_cosp);
-
-        // pitch (y-axis rotation) -> should be assigned to y
-        double sinp = sqrt(1 + 2 * (q.getW() * q.getY() - q.getX() * q.getZ()));
-        double cosp = sqrt(1 - 2 * (q.getW() * q.getY() - q.getX() * q.getZ()));
-        double angleY = 2 * atan2(sinp, cosp) - PI / 2;
-
-        // yaw (z-axis rotation) -> should be assigned to x
         double siny_cosp = 2 * (q.getW() * q.getZ() + q.getX() * q.getY());
         double cosy_cosp = 1 - 2 * (q.getY() * q.getY() + q.getZ() * q.getZ());
         double angleZ = atan2(siny_cosp, cosy_cosp);
